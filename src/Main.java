88c1950797e4ff9b0d59dbfee6120bcdf72daa40
Prestonf136 <prestonfebis100@gmail.com>
2019-11-01 12:32:57 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+//finally got a blank screen setup
+
+/*
+todo:
+-play,pause,stop buttons
+-API
+-graphics
+-animations
+-bug testing
+-polishing
+*/
+
+import javax.swing.*;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
+public class Main {
+  public static void main(String[] args) {
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int[] screen = {
+      (int) screenSize.getWidth(),
+      (int) screenSize.getHeight()
+    };
+
+    JFrame f = new JFrame();
+
+    f.setSize(screen[0],screen[1]);
+    f.setLayout(null);
+    f.setVisible(true);
+  }
 }
